c348c9b669f5bc2929406cc49f4a14c563cd3f67
princejain9294 <princejain9294@gmail.com>
2026-05-09 11:00:39 +0000

    variable

---
+++ JavaScript/variable.js
@@ -0,0 +1,8 @@
+const acccountId = 12345
+let acccountEmail = "prince@gmail.com"
+var accountPass = "Pj9294"
+accountCity = "Bhopal"
+let accountstate;
+
+console.log(acccountId)
+console.table([acccountEmail , acccountId , accountPass , accountCity , accountstate])
