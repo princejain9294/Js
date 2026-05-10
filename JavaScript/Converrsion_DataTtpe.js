bcbfcfedaf9640ecd351295c1ed6fb1c065cf75b
princejain9294 <princejain9294@gmail.com>
2026-05-11 03:58:55 +0530

    Implement type conversion examples in JavaScript
    Add type conversion examples for numbers and booleans.

---
+++ JavaScript/Converrsion_DataTtpe.js
@@ -0,0 +1,23 @@
+// let score = "as"
+// console.log(typeof(score))
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// // "33" = 33
+// // "33a" = NaN
+// // true = 1 , false = 0
+
+// let isLogenIn = Boolean (1) 
+// console.log(isLogenIn);
+
+// 1 = true , 0 = false;
+// "" = false , else true
+
+let sumNum = 33
+let StringNum = String (sumNum)
+console.log(StringNum);
+console.log(typeof StringNum);
+
