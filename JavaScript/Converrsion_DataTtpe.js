acc3179ab42dfe960ef3eefc5ebbeebb7f029abc
princejain9294 <princejain9294@gmail.com>
2026-05-11 04:34:46 +0530

    Clean up Conversion_DataType.js
    Removed commented-out code and added spacing for clarity.

--- JavaScript/Converrsion_DataTtpe.js
+++ JavaScript/Converrsion_DataTtpe.js
@@ -21,3 +21,39 @@ let StringNum = String (sumNum)
 console.log(StringNum);
 console.log(typeof StringNum);
 
+
+
+//**************************operations********************************/
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " Prince"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+
+//console.log(+true);
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(++gameCounter);
+
+
+
+
+
