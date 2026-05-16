6b2a5e47c46392430d6d3c549fe0e11e9e673c9b
princejain9294 <princejain9294@gmail.com>
2026-05-17 03:30:44 +0530

    Add Number and Math utility functions

---
+++ JavaScript/Number_and_Maths.js
@@ -0,0 +1,29 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 158.78967
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//---------------------------------Maths-------------------------------------------
+// same as java
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.543));
+// console.log(Math.ceil(4.543));
+// console.log(Math.floor(4.543));
+// console.log(Math.round(Math.random()*100));
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
