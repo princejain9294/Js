d9522c71c2b0257d1fe704214343d35116baf699
princejain9294 <princejain9294@gmail.com>
2026-05-30 23:12:05 +0530

    Create Array.js with array examples
    Add JavaScript file with array examples and comments

---
+++ JavaScript/Array.js
@@ -0,0 +1,13 @@
+// arrays
+// it contins diff data type ele
+
+const myArray = [0 , 1 , 2 , 3 , 4 , 5]
+// console.log(myArray[5]);
+const MyHeros = ["Eren" , "Tanjiro" , "Levi" , "Yuji"]
+
+const myArr2 = new Array(1 , 2, 3 ,4 ,5)
+// properties
+
+myArray.push(6);
+myArray.
+console.log(myArray);
