24072d730800ec7e19cc86d4152d83db6afa2979
princejain9294 <princejain9294@gmail.com>
2026-05-30 23:15:06 +0530

    Refactor array definitions and improve readability
    Refactor array variable names and clean up comments.

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -1,13 +1,39 @@
-// arrays
-// it contins diff data type ele
+// array
 
-const myArray = [0 , 1 , 2 , 3 , 4 , 5]
-// console.log(myArray[5]);
-const MyHeros = ["Eren" , "Tanjiro" , "Levi" , "Yuji"]
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
 
-const myArr2 = new Array(1 , 2, 3 ,4 ,5)
-// properties
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
 
-myArray.push(6);
-myArray.
-console.log(myArray);
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
