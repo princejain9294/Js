c9e292f1f28c84f0bd14f13733bafa58415cde68
princejain9294 <princejain9294@gmail.com>
2026-07-17 00:27:24 +0530

    Add array manipulation examples in all_about_array.js
    Added various array manipulation methods as comments for reference.

---
+++ JavaScript/all_about_array.js
@@ -0,0 +1,49 @@
+let arr = [1 , 2 , 3 , 4 , 5 , 6];
+// arr.push(7);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+// arr.unshift(0);
+// console.log(arr);
+// arr.splice(2 ,2); // splice mai 2 no. pass karte hai kis idx se delete karna hai or kitna delete karna hai
+// console.log(arr);
+// let newarr1 = arr.slice(0 ,3)
+// console.log(newarr1);
+// arr.reverse();
+// console.log(arr);
+// arr.sort(function(a , b){
+//     return b - a; // a - b for acc and b - a for dec....
+// });
+// arr.forEach(function(val){
+//     console.log(val);
+// })
+
+// let newarr = arr.map(function(val){
+//     return 12;
+// })
+// console.log(newarr);
+
+// let newarr = arr.filter(function(val){
+//     if(val > 4) return true;
+// })
+
+// let ans = arr.reduce(function(val1 , val2){
+//     return val1+val2;
+// })
+// console.log(ans)
+
+// let ans = arr.find(function(val) {
+//     return val === 1
+// })
+// console.log(ans);
+
+// let ans = arr.some(function(val){
+//     return val > 4
+// })
+
+// let ans = arr.every(function(val){
+//     return val > 5
+// })
+// console.log(ans)
