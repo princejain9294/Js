8da110faa097cda1305fed01dc57d6afda74b339
princejain9294 <princejain9294@gmail.com>
2026-07-17 11:16:57 +0530

    Comment out reduce and find function examples
    Commented out examples of reduce and find functions to prevent execution.

--- JavaScript/all_about_array.js
+++ JavaScript/all_about_array.js
@@ -31,7 +31,7 @@ let arr = [1 , 2 , 3 , 4 , 5 , 6];
 
 // let ans = arr.reduce(function(val1 , val2){
 //     return val1+val2;
-// })
+// } , 0)
 // console.log(ans)
 
 // let ans = arr.find(function(val) {
