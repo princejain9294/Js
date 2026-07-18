997d193c693f34b892460d21a31f5f20cf787a8a
princejain9294 <princejain9294@gmail.com>
2026-07-18 11:50:27 +0530

    Delete JavaScript/variable.js

--- JavaScript/variable.js
+++
@@ -1,8 +0,0 @@
-const acccountId = 12345
-let acccountEmail = "prince@gmail.com"
-var accountPass = "Pj9294"
-accountCity = "Bhopal"
-let accountstate;
-
-console.log(acccountId)
-console.table([acccountEmail , acccountId , accountPass , accountCity , accountstate])
